f0281f5335ddb9b7cc0bdce98937d6bf5bbe41f0
ParasGarg19 <156179953+ParasGarg19@users.noreply.github.com>
2024-03-06 14:30:04 +0530

    Updated hi.c

--- hi.c
+++ hi.c
@@ -2,5 +2,12 @@
 
 int main(){
     printf("Helloooo!");
+    int a, b;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    printf("Enter b: ");
+    scanf("%d", &b);
+
+    printf("Sum: %d", a+b);
     return 0;
 }
